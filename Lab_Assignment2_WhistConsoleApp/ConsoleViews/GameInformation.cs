082e03bcd052395ee2109f1f820c6913fd9e08e2
TristanMoe <Trilleo@hotmail.com>
2019-04-10 14:50:25 +0200

    Added GameInformation

--- Lab_Assignment2_WhistConsoleApp/ConsoleViews/GameInformation.cs
+++ Lab_Assignment2_WhistConsoleApp/ConsoleViews/GameInformation.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Lab_Assignment2_WhistConsoleApp.Events;
+using Lab_Assignment2_WhistConsoleApp.Repositories;
 
 namespace Lab_Assignment2_WhistConsoleApp.ConsoleViews
 {
@@ -17,17 +19,20 @@ namespace Lab_Assignment2_WhistConsoleApp.ConsoleViews
     public class GameInformation
     {
         public event EventHandler GameCreated;
+        private string Gamename { get; set; }
+        private string[] Firstnames { get; set; }
+        private string[] Lastnames { get; set; }
 
-        protected virtual void OnGameCreated(EventArgs e)
+        protected virtual void OnGameCreated(GameInformationEventArg e)
         {
             EventHandler handler = GameCreated;
             handler?.Invoke(this, e);
         }
 
 
-        public GameInformation(EventHandler eventsub)
+        public GameInformation(StartPageView viewSubscribe)
         {
-
+            
         }
 
         public void GameStarted(object sender, EventArgs e)
@@ -39,7 +44,39 @@ namespace Lab_Assignment2_WhistConsoleApp.ConsoleViews
         {
             while (true)
             {
-                Console.WriteLine("Please Enter Your Game Name: ");
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Please Enter Your Game Name: ");
+                    var gamename = Console.ReadLine();
+                    Console.WriteLine("\n Please Enter All Four Players Names:");
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        Console.WriteLine($"Player {i}: ");
+                        Console.Write("Firstname: ");
+                        Firstnames[i] = Console.ReadLine();
+                        Console.Write("\n Lastname: ");
+                        Lastnames[i] = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("\n Press Enter To Start Game");
+
+                    var input = Console.ReadKey(true).Key;
+
+                    if (input == ConsoleKey.Enter)
+                    {
+                        RepoGame.RepoCreateANewGame(Gamename, Firstnames, Lastnames);
+                    }
+                    else
+                    {
+                        throw new Exception("You Must Press Enter To Continue");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
             }
 
         }
