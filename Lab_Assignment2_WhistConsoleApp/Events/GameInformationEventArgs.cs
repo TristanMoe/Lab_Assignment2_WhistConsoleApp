63428f1bc79a6bcd009cc4bd52bbc78763bde624
TristanMoe <Trilleo@hotmail.com>
2019-04-10 14:29:50 +0200

    Added EventArgs

---
+++ Lab_Assignment2_WhistConsoleApp/Events/GameInformationEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lab_Assignment2_WhistPointCalculator;
+
+namespace Lab_Assignment2_WhistConsoleApp.Events
+{
+    public class GameInformationEventArg : EventArgs
+    {
+        public Games Game { get; set; }
+        public List<GamePlayers> GamePlayers { get; set; }
+    }
+}
